d3616fb161d0a6d62b0b5f3c0b723e4064d9c7bc
hho <migely16@gmail.com>
2021-10-28 09:15:35 -0400

    first commit

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,8 @@
+@extends('frontend.layouts.app')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+      <h3>This is Home Page</h3>
+    </div>
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Auth::routes();
+Route::get("/","Frontend\PageController@home");
+Route::get("admin/login","Auth\AdminLoginController@showLoginForm");
+Route::post("admin/login","Auth\AdminLoginController@login")->name("admin.login");
+Route::get("/admin",function(){
+    return "admin page";
+});
+
